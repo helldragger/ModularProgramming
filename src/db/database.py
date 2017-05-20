36ded12b28012c572a4fd5dc06cff57c46f6097c
helldragger <christopher1223334444@live.fr>
2017-05-20 16:41:57 +0200

    Database auto-update implementation

--- src/db/database.py
+++ src/db/database.py
@@ -2,15 +2,42 @@
 The database hardware-to-memory layer
 """
 import os
+import threading
+import time as t
 
 import src.db.errors as err
-
 
 # Database format: { TypeOfAlgorithm1, TypeOfAlgorithm2...}
 # -> TypeOfAlgorithm format: { SpecificAlgorithm1, SpecificAlgorithm2...}
 # -> SpecificAlgorithm format: { Lang1, Lang2, Lang3....}
 # -> Lang format : "Path\\To\\Correspondent\\Specific\\File"
 database = {}
+
+
+class Updater:
+    def __init__(self):
+        self._timer = None
+        self.interval = 0.5
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        update_database()
+
+    def start(self):
+        if not self.is_running:
+            self._timer = threading.Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
+updater = Updater()
 
 
 def detect_language(filename):
@@ -29,12 +56,46 @@ def detect_language(filename):
         return languages[extension]
 
 
+def get_algorithm_string(path):
+    """
+    Returns the content of the algorithm contained in the file given in parameter
+    :param path: the file path
+    :return: a string of the algorithm
+    """
+    with open(path, 'r') as file:
+        string = "".join(file.readlines())
+        return string
+
+
+def get_algorithm(algo_type, algo_spec, algo_lang):
+    """
+    Evaluate the query asked
+    :param algo_type: the type of algorithm (the directory)
+    :param algo_spec: the specific algorithm (the filename)
+    :param algo_lang: the language of the specific algorithm (extension)
+    :return: the result
+    """
+    updater.stop()
+    while updater.is_running:
+        t.sleep(0.1)
+    updater.can_update = False
+    if database.get(algo_type) is None:
+        return err.type_not_found()
+    if database[algo_type].get(algo_spec) is None:
+        return err.specific_not_found()
+    if database[algo_type][algo_spec].get(algo_lang) is None:
+        return err.language_not_found(algo_lang)
+    data = get_algorithm_string(database[algo_type][algo_spec][algo_lang])
+    updater.start()
+    return data
+
+
 def update_database():
     """
     Update the database with the current database state.
     :return: nothing
     """
-
+    threading.Timer(1.0, update_database()).start()
     # verify if all the cached files still exists
     dirs_to_verify = database.keys()
     for d in dirs_to_verify:
@@ -74,31 +135,3 @@ def update_database():
             if database[type_string][specific_string].get(language) is None:
                 database[type_string][specific_string][language] = "\\".join([type_dir, file_name])
     return
-
-
-def get_algorithm_string(path):
-    """
-    Returns the content of the algorithm contained in the file given in parameter
-    :param path: the file path
-    :return: a string of the algorithm
-    """
-    with open(path, 'r') as file:
-        string = "".join(file.readlines())
-        return string
-
-
-def get_algorithm(algo_type, algo_spec, algo_lang):
-    """
-    Evaluate the query asked
-    :param algo_type: the type of algorithm (the directory)
-    :param algo_spec: the specific algorithm (the filename)
-    :param algo_lang: the language of the specific algorithm (extension)
-    :return: the result
-    """
-    if database.get(algo_type) is None:
-        return err.type_not_found()
-    if database[algo_type].get(algo_spec) is None:
-        return err.specific_not_found()
-    if database[algo_type][algo_spec].get(algo_lang) is None:
-        return err.language_not_found(algo_lang)
-    return get_algorithm_string(database[algo_type][algo_spec][algo_lang])
