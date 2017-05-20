c4c79b7e36634bd9d875e8a960dca1c1441f77ee
helldragger <christopher1223334444@live.fr>
2017-05-21 00:01:13 +0200

    tests updated

--- tests/db/test_db.py
+++ tests/db/test_db.py
@@ -1,16 +1,6 @@
 import src.db.manager as db
 
 
-def test_start():
-    assert db.start_database() is None
-
-
-def test_stop():
-    db.start_database()
-    assert db.stop_database() is None
-
-
 def test_query():
-    db.start_database()
     with open('tests/db/simple_db_example.py', 'r') as test_file:
         assert db.ask_database("SORT SIMPLE PYTHON".split(' ')) == "".join(test_file.readlines())
